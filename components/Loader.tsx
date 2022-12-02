331a686f43372dc056d4a3a593fe59b402ae7015
Youness Masaoudi <marvelbark2@gmail.com>
2022-12-02 01:23:21 +0100

    Adding Loader

---
+++ components/Loader.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image"
+export default function Loader() {
+    return (
+        <div className="flex h-screen bg-gray-100">
+            <div className="flex flex-col m-auto items-center space-y-4">
+                <Image src="/assets/loader.png"
+                    alt="loading"
+                    className="animate-spin"
+                    width={200}
+                    height={200}
+                />
+                <span className="text-bloody-red font-extrabold text-lg uppercase bg-red-100 px-2 py-1">Utilises un préservatif</span>
+            </div>
+        </div>
+    )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,49 @@ import '@fortawesome/fontawesome-svg-core/styles.css'
 import type { AppProps } from 'next/app';
 import { config } from '@fortawesome/fontawesome-svg-core'
 import Layout from '../components/Layout';
+import { useEffect, useState } from 'react';
+import Loader from 'components/Loader';
+import { useRouter } from 'next/router';
 
 config.autoAddCss = false
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      setLoading(true);
+    }
+
+    const handleRouteDone = () => {
+      const timer = setTimeout(() => {
+        setLoading(false);
+      }, 1000)
+
+      return () => clearTimeout(timer);
+    }
+
+    router.events.on('routeChangeStart', handleRouteChange);
+    router.events.on('routeChangeComplete', handleRouteDone());
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange);
+      router.events.off('routeChangeComplete', handleRouteDone());
+    }
+  }, [router]);
+
+  if (loading) return <Loader />
   return <Layout>
     <Component {...pageProps} />
   </Layout>
